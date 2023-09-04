c57f40438ae46de18c6549171aed50e992496dcb
raygeeknyc <raygeeknyc@gmail.com>
2023-09-04 16:18:39 -0400

    Speech tagging is working well NER is working well subprocess shutdown is broken

--- speechrecognizer.py
+++ speechrecognizer.py
@@ -9,7 +9,8 @@ import io
 import json
 import multiprocessing
 from multiprocessingloghandler import ParentMultiProcessingLogHandler, ChildMultiProcessingLogHandler
-import nltk
+from nltk import ne_chunk, pos_tag, word_tokenize
+from nltk.tree import Tree
 import os
 import queue
 import sys
@@ -22,14 +23,15 @@ SAMPLE_RATE = 44100
 class SpeechRecognizer(multiprocessing.Process):
 
     def tag_speech(phrase:str) -> list:
-        tagged_tokens = nltk.pos_tag(nltk.word_tokenize(phrase.strip()))
+        tagged_tokens = pos_tag(word_tokenize(phrase.strip()))
         return tagged_tokens
 
     def tag_names(tokens:list) -> list:
         names = []
-        chunks = nltk.ne_chunk(tokens)
+        chunks = ne_chunk(tokens)
         for chunk in chunks:
-            if type(chunk) == nltk.tree.Tree:
+            print('{}: {}'.format(type(chunk),str(chunk)))
+            if type(chunk) == Tree:
                 name = ''
                 for chunk_leaf in chunk.leaves():
                     name += chunk_leaf[0] + ' ' 
@@ -42,12 +44,13 @@ class SpeechRecognizer(multiprocessing.Process):
         self._logging_level = logging_level
         self._exit = multiprocessing.Event()
         self.is_ready = multiprocessing.Event()
+        self._exit.clear()
+        self.is_ready.clear()
         self._transcript, _ = transcript
         self._stop_capturing = False
         self._stop_recognizing = False
         self._audio_packet_queue = queue.Queue()
         self.model = KaldiRecognizer(Model(lang="en-us"), SAMPLE_RATE)
-        self.is_ready.clear()
 
     def stop(self):
         logging.debug("***background received shutdown")
@@ -62,23 +65,22 @@ class SpeechRecognizer(multiprocessing.Process):
         self._initLogging()
         import sounddevice as sd  # pyaudio has problems in multiprocesses, this works around that
         self.sd = sd
-        try:
-            logging.debug("recognizer process active")
-            self._capturer = threading.Thread(target=self.captureSound)
-            self._recognizer = threading.Thread(target=self.recognizeSpeech)
-            self._recognizer.start()
-            self._capturer.start()
-            self._exit.wait()
-        except Exception:
-            logging.exception("recognizer process exception")
-        finally:
-            self._stopCapturing()
-            self._stopRecognizing()
-            self._capturer.join()
-            self._recognizer.join()
-            self._transcript.close()
-            logging.debug("recognizer process terminating")
-            sys.exit(0)
+        logging.debug("recognizer process active")
+        self._capturer = threading.Thread(target=self.captureSound)
+        self._recognizer = threading.Thread(target=self.recognizeSpeech)
+        self._recognizer.start()
+        self._capturer.start()
+        self.is_ready.set()
+        logging.debug('speechrecognizer waiting for stop()')
+        self._exit.wait()  # block until stop() is invoked
+        logging.debug('speechrecognizer saw stop()')
+        self._stopCapturing()
+        self._stopRecognizing()
+        self._capturer.join()
+        self._recognizer.join()
+        self._transcript.close()
+        logging.debug("recognizer process terminating")
+        sys.exit(0)
 
     def _stopCapturing(self):
         logging.debug("setting stop_capturing")
@@ -104,6 +106,12 @@ class SpeechRecognizer(multiprocessing.Process):
         logging.info("stopped capturing")
 
     def recognizeSpeech(self):
+        logging.info("starting recognizing")
+        phrase = 'What does John Doe do in the morning'
+        tagged_tokens = SpeechRecognizer.tag_speech(phrase)
+        print(tagged_tokens)
+        names = SpeechRecognizer.tag_names(tagged_tokens)
+        print(names)
         while not self._stop_recognizing:
             packet = self._audio_packet_queue.get()
             if self.model.AcceptWaveform(packet):
@@ -116,6 +124,7 @@ class SpeechRecognizer(multiprocessing.Process):
             else:
                 snippet=json.loads(self.model.PartialResult())['partial']
                 print(str(snippet)+'...')
+        logging.info("stopped recognizing")
  
 
 def main(unused):
@@ -135,16 +144,15 @@ def main(unused):
     
     logging.debug("Waiting for speech recognizer to be ready")
     recognition_worker.is_ready.wait()
-    for _ in range(2):
-        print("listening for %d seconds" % TEST_RESUME_SECS)
-        time.sleep(TEST_RESUME_SECS)
-        print("pausing for %d seconds" % TEST_SUSPEND_SECS)
-        recognition_worker.suspendListening()
-        time.sleep(TEST_SUSPEND_SECS)
-        recognition_worker.resumeListening()
-
-    recognition_worker.stop()
-    recognition_worker.join()
+    try:
+        logging.debug("Passing in main process")
+        while True:
+            pass
+    except Exception:
+        print('Exit exception')
+    finally:
+        recognition_worker.stop()
+        recognition_worker.join()
 
 if __name__ == '__main__':
     print('running standalone recognizer')
